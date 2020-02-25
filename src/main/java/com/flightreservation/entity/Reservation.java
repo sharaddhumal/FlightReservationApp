778c84c674d03525e6ae3ac400fb5606f47048c6
sharad dhumal <sharaddhumal25@gmail.com>
2020-02-25 16:46:53 +0530

    Add @Column annotations to Reservation entity

--- src/main/java/com/flightreservation/entity/Reservation.java
+++ src/main/java/com/flightreservation/entity/Reservation.java
@@ -1,15 +1,21 @@
 package com.flightreservation.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 
 @Entity
 public class Reservation extends AbstractEntity  {
 
+	@Column(name = "CHECKED_IN")
 	private Boolean checkedIn;
+	@Column(name = "NUMBER_OF_BAGS")
 	private int numberOfBags;
+	//@Column(name = "PASSENGER_ID")
 	@OneToOne
 	private Passenger passenger;
+	
+	//@Column(name = "FLIGHT_ID")
 	@OneToOne
 	private Flight flight;
 
@@ -44,4 +50,11 @@ public class Reservation extends AbstractEntity  {
 	public void setFlight(Flight flight) {
 		this.flight = flight;
 	}
+
+	@Override
+	public String toString() {
+		return "Reservation [checkedIn=" + checkedIn + ", numberOfBags=" + numberOfBags + ", passenger=" + passenger
+				+ ", flight=" + flight + "]";
+	}
+	
 }
